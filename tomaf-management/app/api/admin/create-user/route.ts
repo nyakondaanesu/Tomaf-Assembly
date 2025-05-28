0955d3e356deb80830a038dc292b524ba61c0873
nyakonda anesu <nyakondaanesu@gmail.com>
2025-05-28 15:01:13 +0200

    linting 2

--- tomaf-management/app/api/admin/create-user/route.ts
+++ tomaf-management/app/api/admin/create-user/route.ts
@@ -10,7 +10,11 @@ export async function POST(req: Request) {
 
     const user = await createUserCredentials(username, password, role);
     return new Response(JSON.stringify(user), { status: 201 });
-  } catch (err: any) {
-    return new Response(err.message, { status: 500 });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error("Error fetching age data:", err.message);
+    } else {
+      console.error("Unknown error fetching age data:", err);
+    }
   }
 }
